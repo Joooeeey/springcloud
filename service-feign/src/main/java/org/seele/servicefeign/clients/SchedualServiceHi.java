df87ebdb9251644d8714602ecd25c0161b5c9f61
JoeySH <Joey_Shen_struggle@163.com>
2018-08-28 10:16:14 +0800

    添加负载均衡 feign

---
+++ service-feign/src/main/java/org/seele/servicefeign/clients/SchedualServiceHi.java
@@ -0,0 +1,18 @@
+package org.seele.servicefeign.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Description: 定义一个feign接口，通过@ FeignClient（“服务名”），来指定调用哪个服务。
+ * @author Joey 
+ * @date: 2018/8/28
+ */
+
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
---
+++ service-feign/src/main/java/org/seele/servicefeign/controller/HiController.java
@@ -0,0 +1,25 @@
+package org.seele.servicefeign.controller;
+
+import org.seele.servicefeign.clients.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: 对外暴露一个”/hi”的API接口，通过Feign客户端SchedualServiceHi 来消费服务
+ * @author Joey 
+ * @date: 2018/8/28
+ */
+
+@RestController
+public class HiController {
+    //编译器报错，无视。 因为这个Bean是在程序启动的时候注入的，编译器感知不到，所以报错。
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @GetMapping(value = "/hi")
+    public String sayHi(@RequestParam String name) {
+        return schedualServiceHi.sayHiFromClientOne( name );
+    }
+}
